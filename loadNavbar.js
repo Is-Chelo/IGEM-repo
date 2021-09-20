a347b361ee3fbf69f87430ed5bd3e5d6868c2eb6
Carlos Marcelo Torres Vargas <carlosmarcelotorresvargas@gmail.com>
2021-09-20 15:02:27 -0400

    MEjorando la responsividad

---
+++ loadNavbar.js
@@ -0,0 +1,9 @@
+window.addEventListener('scroll', (e)=>{
+    let y = window.scrollY;
+    if(y >= 500){
+        document.getElementById('nav').classList.add('my-nav')
+    }else{
+        document.getElementById('nav').classList.remove('my-nav')
+    }
+})
+
