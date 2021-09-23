2c579bf7661dda0a5d259e816ed66c7414d5e819
Carlos Marcelo Torres Vargas <carlosmarcelotorresvargas@gmail.com>
2021-09-22 21:20:17 -0400

    Final

--- loadNavbar.js
+++ loadNavbar.js
@@ -1,9 +1,13 @@
+
 window.addEventListener('scroll', (e)=>{
     let y = window.scrollY;
     if(y >= 500){
         document.getElementById('nav').classList.add('my-nav')
     }else{
         document.getElementById('nav').classList.remove('my-nav')
-    }
+    } 
 })
 
+function navBarColor(){
+    document.getElementById('nav').classList.add('my-nav')
+}
